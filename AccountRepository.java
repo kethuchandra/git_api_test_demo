e48fcd1d3fa795aefa32b4529ab9cd306cfb59e5
kethuchandra <149141397+kethuchandra@users.noreply.github.com>
2024-01-25 12:03:29 +0530

    Update AccountRepository.java

--- AccountRepository.java
+++ AccountRepository.java
@@ -2,10 +2,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-
-    // Method to find an account by account number
     Account findByAccountNumber(String accountNumber);
-
-    // Additional methods for custom queries related to accounts, if needed
-    // For example: List<Account> findByBalanceGreaterThan(double balance);
 }
